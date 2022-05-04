d3e58c27fae4e8cd9c0fe608cbaf6a9280353c09
cantugba <tugbacan57@outlook.com>
2022-05-04 23:10:06 +0300

    state pattern Media Player example on JAVA

---
+++ Behavioral/State/Java/State/src/MediaPlayer/Demo.java
@@ -0,0 +1,10 @@
+package MediaPlayer;
+
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+
+        ui.init();
+    }
+}
